e3c9495062cf4e11fb4a774d9e791a82e450c505
Mike Decker <pookmish@gmail.com>
2017-12-13 15:49:15 -0800

    cahnged auth sources

--- simplesamlphp/config/authsources.php
+++ simplesamlphp/config/authsources.php
@@ -22,12 +22,14 @@ $config = array(
 
         // The entity ID of the IdP this should SP should contact.
         // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
-        'idp' => null,
+        'idp' => 'https://idp.stanford.edu/',
 
         // The URL to the discovery service.
         // Can be NULL/unset, in which case a builtin discovery service will be used.
         'discoURL' => null,
 
+        'privatekey'           => 'saml.pem',
+        'certificate'          => 'saml.crt',
         /*
          * WARNING: SHA-1 is disallowed starting January the 1st, 2014.
          *
