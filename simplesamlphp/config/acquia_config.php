e02ecea6b2d3f08ba8f02ffd6b627895a4f21b4d
pookmish <pookmish@stanford.edu>
2017-12-15 10:33:24 -0800

    Mrcd8 36 saml config (#8)
    * MRCD8-36 Initiated saml
    
    * changed saml cert path
    
    * htaccess change domain
    
    * saml metadata
    
    * cahnged auth sources
    
    * moved config to acquia server
    
    * added attribute map
    
    * changed htaccess patch
    
    * removed debug line in deploy hook

---
+++ simplesamlphp/config/acquia_config.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists('/home/hsmrc/saml/acquia_configs.php')) {
+  include_once '/home/hsmrc/saml/acquia_configs.php';
+}
---
+++ simplesamlphp/metadata/saml20-sp-remote.php
@@ -0,0 +1,6 @@
+<?php
+
+// Load file on acquia server.
+if (file_exists('/home/hsmrc/saml/saml20-sp-remote.php')) {
+  include_once '/home/hsmrc/saml/saml20-sp-remote.php';
+}
